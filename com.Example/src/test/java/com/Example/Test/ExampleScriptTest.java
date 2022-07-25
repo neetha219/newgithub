ee64688fa850831f6c6c522ff86d4e06656be270
neethashree93@gmail.com <neethashree93@gmail.com>
2022-07-25 13:16:20 +0530

    very new

---
+++ com.Example/src/test/java/com/Example/Test/ExampleScriptTest.java
@@ -0,0 +1,21 @@
+package com.Example.Test;
+
+import org.testng.annotations.Test;
+
+public class ExampleScriptTest {
+	@Test
+     public static void ExampleTest()
+	{
+		System.out.println("Goodbye");
+	}
+	@Test
+	public static void ExampleTest3()
+	{
+		System.out.println("new");
+		
+	}
+    public static void ExampleTest4()
+    {
+    	System.out.println("checked");
+    }
+}
